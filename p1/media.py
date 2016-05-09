8b65fe0f96d0588f0e0dc98f902f98b52e4e3641
Jared Lewis <jslewis90@outlook.com>
2016-05-09 14:34:09 -0400

    Update media.py

--- p1/media.py
+++ p1/media.py
@@ -12,7 +12,9 @@ class TvShow(Video):
 
     def __init(self, show_title, show_length, show_season, show_episode,
                tv_station):
+        # Call on the Video parent class to initialize the title and length
         Video.__init__(self, show_title, show_length)
+        # Initializes the show's season, episode number, and TV station
         self.season = show_season
         self.episode = show_episode
         self.station = tv_station
@@ -23,12 +25,17 @@ class Movie(Video):
     
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube, movie_length, movie_rating):
+        # Call on the Video parent class to initialize the title and length
         Video.__init__(self, movie_title, movie_length)
+        # Initializes the storyline, poster image, and YouTube trailer
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        # Compares the entered rating against the defined Valid Ratings
         for rating in Movie.VALID_RATINGS:
             if rating == movie_rating:
                 self.rating = movie_rating
+
     def show_trailer(self):
+        # Opens the Movie's youtube trailer in a web browser
         webbrowser.open(self.trailer_youtube_url)
